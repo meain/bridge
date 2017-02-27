7b7120fa7d5b7bf6f7c71c5c6979ff6f2045fec5
Abin Simon <abinsimon10@gmail.com>
2017-02-27 15:45:55 +0530

    Sarted with handling timetable data

--- core/static/core/js/index.js
+++ core/static/core/js/index.js
@@ -6,7 +6,77 @@ var user_data = {
 
 
 var timetable = {
-    'monday': {'maths', 'physics', 'chemistry', 'computer science', 'party', 'none'}
+    'monday': ['maths', 'physics', 'chemistry', 'computer science', 'party', 'boohoo'],
+    'tuesday': ['maths', 'physics', 'chemistry', 'computer science', 'party', 'boohoo'],
+    'wednessday': ['maths', 'physics', 'chemistry', 'computer science', 'party', 'boohoo'],
+    'thursday': ['maths', 'physics', 'chemistry', 'computer science', 'party', 'boohoo'],
+    'friday': ['maths', 'physics', 'chemistry', 'computer science', 'party', 'boohoo']
+}
+
+
+//attendece and stuff
+var track_data = {
+    '10-12-2017':[
+        {
+            'period': 0, //period number
+            'notes': 'random notes in period 0', //note data
+            'attendece' : '' // bunked|free|attended
+        },
+        {
+            'period': 1, //period number
+            'notes': '', //note data
+            'attendece' : '' // bunked|free|attended
+        },
+        {
+            'period': 2, //period number
+            'notes': '', //note data
+            'attendece' : '' // bunked|free|attended
+        },
+        {
+            'period': 3, //period number
+            'notes': '', //note data
+            'attendece' : '' // bunked|free|attended
+        },
+        {
+            'period': 4, //period number
+            'notes': '', //note data
+            'attendece' : '' // bunked|free|attended
+        },
+        {
+            'period': 5, //period number
+            'notes': '', //note data
+            'attendece' : '' // bunked|free|attended
+        }
+    ]
+}
+
+
+// Will contain all the data for all the subjects that a student has
+var subject_data = {
+    'maths': {
+        'subject': 'Mathematics',  // incase we have abbr, probably won't get to that
+        'teacher':'Adam Smith',
+    },
+    'physics': {
+        'subject': 'Physics',
+        'teacher':'Delin Mathew',
+    },
+    'chemistry': {
+        'subject': 'Chemistry',
+        'teacher':'Alex Philip',
+    },
+    'computer science': {
+        'subject': 'Computer Science',
+        'teacher':'Hebin Hentry',
+    },
+    'party': {
+        'subject': 'Partttyyy',
+        'teacher':'meain',
+    },
+    'boohoo': {
+        'subject': 'BoooooHoooo',
+        'teacher':'Boo',
+    }
 }
 
 
@@ -54,8 +124,29 @@ populate_user_profile = function(user){
     $('#profile-name').html(user['name']);
     $('#profile-image').attr('src', user['photo']);
 }
+populate_timetable_heder = function(timetable){
+    var count = 0;
+    for (var i in timetable) {
+       if (timetable.hasOwnProperty(i)) count++;
+    }
+    htmlstr = ''
+    var day = new Date().getDay();
+    var dow = ['sunday', 'monday', 'tuesday', 'wednessday', 'thursday', 'friday', 'saturday'][day]
+    for( var i=0; i<=count; i++ ){
+        htmlstr+=
+            "<div class='card tiny timetable-subject'>"+
+                timetable[dow][i]+
+            "</div>"
+    }
+    htmlstr =
+        "<div id='timetable-banner'>"+
+            htmlstr+
+        "</div>"
+    $('.midpane.pane').append(htmlstr);
+}
 populate_upcoming(upcoming_events);
 populate_user_profile(user_data);
+populate_timetable_heder(timetable);
 
 
 
