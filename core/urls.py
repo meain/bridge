3f6985452848f016e089b89b928e09698084339c
Abin Simon <abinsimon10@gmail.com>
2017-03-01 19:16:03 +0530

    example views, get and post. csrf disabled for now

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,11 @@
 from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls.static import static
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = "index"), # initial request for page
+    # http://localhost:8000/upcoming/12344/
+    url(r'upcoming/(?P<user_id>[0-9]+)/$', views.signin, name = "signin"),
+    # curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://localhost:8000/downcoming/1234/
+    # actual request will be with content json as in js
+    url(r'downcoming/(?P<user_id>[0-9]+)/$', views.something_post, name = "shi"),
 ]
-
-# if settings.DEBUG: # change media serving based on debug flag
-#     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-# urlpatterns = format_suffix_patterns(urlpatterns)
--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-	# Return the page at ai/index.html
-	return render(request, "core/index.html", None)
+    # Return the page at ai/index.html
+    return render(request, "core/index.html", None)
 
+
+def signin(request, user_id):
+    data = 'Supposed to return data for ' + str(user_id)
+    return HttpResponse(data)
+
+def something_post(request, user_id):
+    print request.body
+    return HttpResponse('eeks')
