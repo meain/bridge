ea49d060540c5b71f900e08c1e7c7eb3d26e8bb9
abhai <abhai.dilip@gmail.com>
2017-03-01 23:24:15 +0530

    Added models --EXTREMELY immature version

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+import models
 # Register your models here.
+myModels = [models.Student,
+			models.Teacher,
+			models.Department,
+			models.Subject,
+			models.Class,
+			models.Events]
+
+admin.site.register(myModels)
--- core/models.py
+++ core/models.py
@@ -3,54 +3,63 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+sem_choices = (
+	(1,'S1'),
+	(2,'S2'),
+	(3,'S3'),
+	(4,'S4'),
+	(5,'S5'),
+	(6,'S6'),
+	(7,'S7'),
+	(8,'S8'),
+)
 
 class Student(models.Model):
-	roll_no = models.SmallIntegerField() #For reference only, database primary is key different
-	stud_name = models.CharField(max_length=40)
-	current_class = models.ForiegnKey('Class', on_delete=models.CASCADE)
-	department = models.ForiegnKey('Department', on_delete=models.CASCADE)
-	
+	roll_no = models.SmallIntegerField(verbose_name="Roll no.") #For reference only, database primary is key different
+	stud_name = models.CharField(max_length=40,verbose_name="Name")
+	current_class = models.ForeignKey('Class', on_delete=models.CASCADE,verbose_name="Class")
+
 	def __str__(self):
 		return str(self.stud_name)
 
 class Department(models.Model):
-	dep_names = (
-		('CE', 'Civil Engineering'),
-		('CS', 'Computer Science and Engineering'),
-		('EE', 'Electrical and Electronics Engineering'),
-		('EC', 'Electronics and Communications Engineering'),
-		('IT', 'Information Technology'),
-		('SF', 'Fire and Safety Engineering'),
-		('ME', 'Mechanical Engineering'),
-	)
-	dep_name = models.CharField(max_length=60,choices=dep_names)
+	dep_code = models.CharField(max_length=3, verbose_name="Department Code")
+	dep_name = models.CharField(max_length=60, verbose_name="Department Name")
 
 	def __str__(self):
 		return str(self.dep_name)
 
 class Subject(models.Model):
-	subject_code = models.CharField(max_length=6,primary_key=True)
-	subject_title = models.CharField(max_length=60)
-	subject_dep = models.ForiegnKey('Department', on_delete=models.CASCADE)
+	subject_code = models.CharField(verbose_name="Subject Code",max_length=6,primary_key=True)
+	subject_title = models.CharField(max_length=60, verbose_name="Subject Title")
+	subject_dep = models.ForeignKey('Department', on_delete=models.CASCADE,verbose_name="Branch")
+	subject_sem = models.SmallIntegerField(choices=sem_choices, null=True)
 
 	def __str__(self):
-		return str(self.subject_title)
+		return self.subject_title
 
 class Teacher(models.Model):
 	teacher_name = models.CharField(max_length=40)
-	dept = models.ForiegnKey('Department', on_delete=models.CASCADE)
-	subjects = models.ManyToManyField('Subject')
+	dept = models.ForeignKey('Department', on_delete=models.CASCADE)
+	subjects = models.ManyToManyField('Subject', verbose_name="Subjects")
+
+
+	def __str__(self):
+		return self.teacher_name
 
 class Class(models.Model):
-	sem_choices = (
-		(1,'S1'),
-		(2,'S2'),
-		(3,'S3'),
-		(4,'S4'),
-		(5,'S5'),
-		(6,'S6'),
-		(7,'S7'),
-		(8,'S8'),
-	)
 	current_sem = models.PositiveSmallIntegerField(choices=sem_choices)
-	branch = models.ForiegnKey('Department', on_delete=models.CASCADE)
+	branch = models.ForeignKey('Department', on_delete=models.CASCADE)
+	batch  = models.CharField(max_length=1)
+
+	def __str__(self):
+		return str(self.branch.dep_code)+" "+str(self.current_sem)+str(self.batch)
+
+class Events(models.Model):
+	title = models.CharField(max_length=40, verbose_name="Event title")
+	due_date = models.DateField(verbose_name='Due date')
+	event_description = models.TextField(verbose_name='Description')
+	assigned_to = models.ForeignKey('Class', verbose_name='Assigned to')
+
+	def __str__(self):
+		return self.title
